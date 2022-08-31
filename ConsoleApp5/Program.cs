23d53f4b3d5f20caa2fb4ba01d29d291b957b6de
Melany <melanyac260@gmail.com>
2022-08-31 00:05:43 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1V,y1V;
+            int x2V,y2V;
+            int x3V,y3V;
+            int x4V,y4V;
+
+            //VALORES A INGRESAR POR CONSOLA
+            Console.WriteLine("Ingrese la primera coordenada del punto V1 (?, y): ");
+            x1V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda coordenada del punto V1 (x, ?): ");
+            y1V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la primera coordenada del punto V2 (?, y): ");
+            x2V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda coordenada del punto V2 (x, ?): ");
+            y2V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la primera coordenada del punto V3 (?, y): ");
+            x3V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda coordenada del punto V3 (x, ?): ");
+            y3V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la primera coordenada del punto V4 (?, y): ");
+            x4V = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la segunda coordenada del punto V4 (x, ?): ");
+            y4V = int.Parse(Console.ReadLine());
+
+
+            Rectangulo rectangulo = new Rectangulo();
+
+            Vertice v1 = new Vertice
+            {
+                x = x1V,
+                y = y1V
+            };
+
+            Vertice v2 = new Vertice
+            {
+                x = x2V,
+                y = y2V
+            };
+
+            Vertice v3 = new Vertice
+            {
+                x = x3V,
+                y = y3V
+            };
+
+            Vertice v4 = new Vertice
+            {
+                x = x4V,
+                y = y4V
+            };
+
+            rectangulo.v1 = v1;
+            rectangulo.v2 = v2;
+            rectangulo.v3 = v3;
+            rectangulo.v4 = v4;
+
+            Console.WriteLine("El area del rectangulo es: " + rectangulo.GetArea());
+            Console.WriteLine("El perimetro del rectangulo es: " + rectangulo.GetPerimetro());
+
+        }
+    }
+}
---
+++ ConsoleApp5/Rectangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Rectangulo
+    {
+        /*
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+        public int x3 { get; set; }
+        public int y3 { get; set; }
+        public int x4 { get; set; }
+        public int y4 { get; set; }
+        */
+
+        public Vertice v1 { get; set;  }
+        public Vertice v2 { get; set;  }
+        public Vertice v3 { get; set;  }
+        public Vertice v4 { get; set; }
+
+        //CALCULO DEL AREA DEL RECTANGULO
+        public double GetArea()
+        {
+            double base1 = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + Math.Pow(v2.y - v1.y, 2));
+            double altura = Math.Sqrt(Math.Pow(v3.x - v2.x, 2) + Math.Pow(v3.y - v2.y, 2));
+            return base1*altura;
+        }
+
+        //CALCULO DEL PERIMETRO DE UN RECTANGULO
+        public double GetPerimetro()
+        {
+            double base1 = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + Math.Pow(v2.y - v1.y, 2));
+            double altura = Math.Sqrt(Math.Pow(v3.x - v2.x, 2) + Math.Pow(v3.y - v2.y, 2));
+            return (base1 + altura)*2;
+        }
+
+    }
+}
